25ff8b210c69e821d0f9a8da46cf5026bd855654
Jim O'Brien <git@jimmed.net>
2021-12-04 13:17:44 +0000

    chore: add nix-direnv for deno

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Mettle Ops-Tool development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
---
+++ shell.nix
@@ -0,0 +1,5 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+mkShell {
+  buildInputs = [ deno git github-cli ];
+}
