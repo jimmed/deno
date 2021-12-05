d9ee1c870837336c54fa5690983da8270de038e2
Jim O'Brien <git@jimmed.net>
2021-12-05 18:53:10 +0000

    refactor(fp): export Option interface

--- fp/option/mod.ts
+++ fp/option/mod.ts
@@ -1,3 +1,4 @@
+export type { Option } from "./_types.ts";
 export { none } from "./none.ts";
 export { some } from "./some.ts";
 export { isOption, maybe, maybeFalsy, maybeNullish } from "./util.ts";
