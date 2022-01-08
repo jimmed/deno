be55e2ac876680d5c2eee61e091b7cf8a7b4a267
Jim O'Brien <git@jimmed.net>
2022-01-08 17:09:18 +0000

    chore(deps): update std@1.20.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as path from "https://deno.land/std@0.118.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.118.0/fs/mod.ts";
-export * as streams from "https://deno.land/std@0.118.0/streams/conversion.ts";
+export * as path from "https://deno.land/std@0.120.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.120.0/fs/mod.ts";
+export * as streams from "https://deno.land/std@0.120.0/streams/conversion.ts";
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.118.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.120.0/testing/asserts.ts";
 export * as fc from "https://cdn.skypack.dev/fast-check@v2.20.0?dts";
